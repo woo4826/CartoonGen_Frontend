2fce341e80fbf8d2d8e730ce8e745fe8f6a464cb
woo4826 <woo4826@gmail.com>
2023-05-30 23:53:21 +0900

    끝

--- cartoon-gen/src/Gen.jsx
+++ cartoon-gen/src/Gen.jsx
@@ -1,23 +1,23 @@
-import './App.css';
-import React, { useState, } from 'react';
-import VirtualizedSelect from 'react-virtualized-select'
-import SearchComponent from './SearchComponent';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import "./App.css";
+import React, { useState } from "react";
+import VirtualizedSelect from "react-virtualized-select";
+import SearchComponent from "./SearchComponent";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { Buffer } from "buffer";
 
 function Gen() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const decodeBase64 = (base64data) => {
-    let base64ToString = Buffer.from(base64data, "base64").toString()
-    this.setState({ data: base64ToString })
-  }
+    let base64ToString = Buffer.from(base64data, "base64").toString();
+    this.setState({ data: base64ToString });
+  };
 
   const options = [
-    { label: 'lora1', value: 'lora1String' },
-    { label: 'lora2', value: 'lora2String' },
-    { label: 'lora3', value: 'lora3String' },
-    { label: 'lora4', value: 'lora4String' },
+    { label: "lora1", value: "lora1String" },
+    { label: "lora2", value: "lora2String" },
+    { label: "lora3", value: "lora3String" },
+    { label: "lora4", value: "lora4String" },
   ];
   const [image1, setImage1] = useState("");
   const [selectedLora1, setSelecteLora1] = useState("");
@@ -33,42 +33,51 @@ function Gen() {
     console.log(makePrompt(selectedResult2), selectedLora2.value);
     console.log(makePrompt(selectedResult3), selectedLora3.value);
     console.log(makePrompt(selectedResult4), selectedLora4.value);
-    axios.post('http://127.0.0.1:8000/fastapi/make', {
-      prompt: [
-        makePrompt(selectedResult1) + selectedLora1.value,
-        makePrompt(selectedResult2) + selectedLora2.value,
-        makePrompt(selectedResult3) + selectedLora3.value,
-        makePrompt(selectedResult4) + selectedLora4.value
-      ]
-    }).then((response) => {
-      console.log(response);
-      // setImage1(response.data.image1);
-      setImage1(decodeBase64(response.data.images[0]));
-      navigate("/result");
-    }
-    ).catch((error) => {
-      console.log(error);
-      navigate("/result");
-    }
-    );
-  }
+    axios
+      .post("http://127.0.0.1:8000/fastapi/make", {
+        prompt: [
+          makePrompt(selectedResult1) + selectedLora1.value,
+          makePrompt(selectedResult2) + selectedLora2.value,
+          makePrompt(selectedResult3) + selectedLora3.value,
+          makePrompt(selectedResult4) + selectedLora4.value,
+        ],
+      })
+      .then((response) => {
+        console.log(response);
+        // setImage1(response.data.image1);
+        // setImage1(decodeBase64(response.data.images[0]));
+        navigate("/result", {
+          state: {
+            image0: response.data.images[0],
+            image1: response.data.images[1],
+            image2: response.data.images[2],
+            image3: response.data.images[3],
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        // navigate("/result");
+      });
+  };
   const makePrompt = (stringList) => {
     let prompt = "";
     stringList.forEach((string) => {
       prompt += string.value + ", ";
-    })
+    });
     return prompt;
-  }
+  };
   const loraSelector = (selectedLora, setSelecteLora) => {
     return (
-      <div className='lora_select'>
+      <div className="lora_select">
         <VirtualizedSelect
           options={options}
           onChange={(selectValue) => setSelecteLora(selectValue)}
-          value={selectedLora} />
+          value={selectedLora}
+        />
       </div>
-    )
-  }
+    );
+  };
   const handleDeleteResult = (index, sR, setSR) => {
     const updatedResults = sR.filter((_, i) => i !== index);
     setSR(updatedResults);
@@ -76,118 +85,160 @@ function Gen() {
   return (
     <div className="App">
       <h1>카툰 제너레이터</h1>
-      <div className='userInpuWrapper'>
-
-        <div className='cutWrapper_row'>
-          <div className='cutWrapper'>
-            <div className='cut_row'>
-              <div className='cut'>
+      <div className="userInpuWrapper">
+        <div className="cutWrapper_row">
+          <div className="cutWrapper">
+            <div className="cut_row">
+              <div className="cut">
                 <SearchComponent
                   type="place"
                   selectedResult={selectedResult1}
-                  setSelectedResults={setSelectedResults1} />
+                  setSelectedResults={setSelectedResults1}
+                />
               </div>
-              <div className='cut'>
+              <div className="cut">
                 <SearchComponent
                   type="move"
                   selectedResult={selectedResult1}
-                  setSelectedResults={setSelectedResults1} />
+                  setSelectedResults={setSelectedResults1}
+                />
               </div>
             </div>
             <ul>
               {selectedResult1.map((result, index) => (
                 <li key={index}>
                   {result.label} {result.value}
-                  <button onClick={() => handleDeleteResult(index, selectedResult1, setSelectedResults1)}>삭제</button>
+                  <button
+                    onClick={() =>
+                      handleDeleteResult(
+                        index,
+                        selectedResult1,
+                        setSelectedResults1
+                      )
+                    }
+                  >
+                    삭제
+                  </button>
                 </li>
               ))}
             </ul>
             {loraSelector(selectedLora1, setSelecteLora1)}
           </div>
 
-          <div className='cutWrapper'>
-            <div className='cut_row'>
-              <div className='cut'>
+          <div className="cutWrapper">
+            <div className="cut_row">
+              <div className="cut">
                 <SearchComponent
                   type="place"
                   selectedResult={selectedResult2}
-                  setSelectedResults={setSelectedResults2} />
+                  setSelectedResults={setSelectedResults2}
+                />
               </div>
-              <div className='cut'>
+              <div className="cut">
                 <SearchComponent
                   type="move"
                   selectedResult={selectedResult2}
-                  setSelectedResults={setSelectedResults2} />
+                  setSelectedResults={setSelectedResults2}
+                />
               </div>
             </div>
             <ul>
               {selectedResult2.map((result, index) => (
                 <li key={index}>
                   {result.label} {result.value}
-                  <button onClick={() => handleDeleteResult(index, selectedResult2, setSelectedResults2)}>삭제</button>
+                  <button
+                    onClick={() =>
+                      handleDeleteResult(
+                        index,
+                        selectedResult2,
+                        setSelectedResults2
+                      )
+                    }
+                  >
+                    삭제
+                  </button>
                 </li>
               ))}
             </ul>
             {loraSelector(selectedLora2, setSelecteLora2)}
-
           </div>
-
-        </div><div className='cutWrapper_row'>
-          <div className='cutWrapper'>
-            <div className='cut_row'>
-              <div className='cut'>
+        </div>
+        <div className="cutWrapper_row">
+          <div className="cutWrapper">
+            <div className="cut_row">
+              <div className="cut">
                 <SearchComponent
                   type="place"
                   selectedResult={selectedResult3}
-                  setSelectedResults={setSelectedResults3} />
+                  setSelectedResults={setSelectedResults3}
+                />
               </div>
-              <div className='cut'>
+              <div className="cut">
                 <SearchComponent
                   type="move"
                   selectedResult={selectedResult3}
-                  setSelectedResults={setSelectedResults3} />
+                  setSelectedResults={setSelectedResults3}
+                />
               </div>
             </div>
             <ul>
               {selectedResult3.map((result, index) => (
                 <li key={index}>
                   {result.label} {result.value}
-                  <button onClick={() => handleDeleteResult(index, selectedResult3, setSelectedResults3)}>삭제</button>
+                  <button
+                    onClick={() =>
+                      handleDeleteResult(
+                        index,
+                        selectedResult3,
+                        setSelectedResults3
+                      )
+                    }
+                  >
+                    삭제
+                  </button>
                 </li>
               ))}
             </ul>
             {loraSelector(selectedLora3, setSelecteLora3)}
-
           </div>
 
-          <div className='cutWrapper'>
-            <div className='cut_row'>
-              <div className='cut'>
+          <div className="cutWrapper">
+            <div className="cut_row">
+              <div className="cut">
                 <SearchComponent
                   type="place"
                   selectedResult={selectedResult4}
-                  setSelectedResults={setSelectedResults4} />
+                  setSelectedResults={setSelectedResults4}
+                />
               </div>
-              <div className='cut'>
+              <div className="cut">
                 <SearchComponent
                   type="move"
                   selectedResult={selectedResult4}
-                  setSelectedResults={setSelectedResults4} />
+                  setSelectedResults={setSelectedResults4}
+                />
               </div>
             </div>
             <ul>
               {selectedResult4.map((result, index) => (
                 <li key={index}>
                   {result.label} {result.value}
-
-                  <button onClick={() => handleDeleteResult(index, selectedResult4, setSelectedResults4)}>삭제</button>
+                  <button
+                    onClick={() =>
+                      handleDeleteResult(
+                        index,
+                        selectedResult4,
+                        setSelectedResults4
+                      )
+                    }
+                  >
+                    삭제
+                  </button>
                 </li>
               ))}
             </ul>
             {loraSelector(selectedLora4, setSelecteLora4)}
-
           </div>
-
         </div>
       </div>
 
@@ -195,10 +246,7 @@ function Gen() {
         <button onClick={generate}>제너레이트</button>
       </div>
       <div>
-
         <img src={"data:image/jpeg;base64," + image1.data} />
-
-
       </div>
     </div>
   );
--- cartoon-gen/src/Result.jsx
+++ cartoon-gen/src/Result.jsx
@@ -1,3 +1,23 @@
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 function Result() {
+  let location = useLocation();
+  console.log(location);
+  const [image0, setImage0] = useState(location.state.image0);
+  const [image1, setImage1] = useState(location.state.image1);
+  const [image2, setImage2] = useState(location.state.image2);
+  const [image3, setImage3] = useState(location.state.image3);
+  return (
+    <div className="result">
+      <div className="result_row">
+        <img src={"data:image/jpeg;base64," + image0} alt="image1" />
+        <img src={"data:image/jpeg;base64," + image1} alt="image1" />
+      </div>
+      <div className="result_row">
+        <img src={"data:image/jpeg;base64," + image2} alt="image1" />
+        <img src={"data:image/jpeg;base64," + image3} alt="image1" />
+      </div>
+    </div>
+  );
 }
 export default Result;
