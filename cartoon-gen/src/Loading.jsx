d6f108f43067c6ab7e4b1e1171281393617ef810
binllionaire <bug20474@gmail.com>
2023-06-02 09:20:58 +0900

    로딩 스피너 추가

--- cartoon-gen/src/Loading.jsx
+++ cartoon-gen/src/Loading.jsx
@@ -1,6 +1,13 @@
+import spinner from "./img/spinner.gif"
+
 function Loading () {
 return(
-    <div>AI가 만화를 생성하고 있어요</div>
+    <div className="Loading">
+    <div className="Loading-Title">AI가 만화를 생성하고 있어요</div>
+    <div className="spinner-wrapper">
+        <img id="spinner" src={spinner}></img>
+    </div>
+    </div>
 )
 }
 export default Loading;
