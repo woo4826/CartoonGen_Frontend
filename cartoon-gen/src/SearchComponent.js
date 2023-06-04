77e20762b16fc2bfc022b7c80c96b631ae276b28
haruachm <85959639+haruachm@users.noreply.github.com>
2023-06-04 15:36:22 +0900

    feat: 배경선택 체크박스 클릭 -> 장소 드롭박스 비활성화

--- cartoon-gen/src/SearchComponent.js
+++ cartoon-gen/src/SearchComponent.js
@@ -1,40 +1,60 @@
-import React, { useState } from 'react';
-import placeData from './data/place.json';
-import moveData from './data/move.json';
-import VirtualizedSelect from 'react-virtualized-select'
+import React, { useState } from "react";
+import placeData from "./data/place.json";
+import moveData from "./data/move.json";
+import VirtualizedSelect from "react-virtualized-select";
 
-import 'react-select/dist/react-select.css'
-import 'react-virtualized/styles.css'
-import 'react-virtualized-select/styles.css'
+import "react-select/dist/react-select.css";
+import "react-virtualized/styles.css";
+import "react-virtualized-select/styles.css";
 
 const SearchComponent = (props) => {
-    var data = props.type === "move" ? moveData : placeData;
-    const selectedResult = props.selectedResult;
-    const setSelectedResults = props.setSelectedResults;
-    const setString = props.setString;
-    const options = data.list;
-    const [selectValue, setSelectedValue] = useState([]);
+  var data = props.type === "move" ? moveData : placeData;
+  const selectedResult = props.selectedResult;
+  const setSelectedResults = props.setSelectedResults;
+  const setString = props.setString;
+  const options = data.list;
+  const [selectValue, setSelectedValue] = useState([]);
+  const [backgroundSelected, setBackgroundSelected] = useState(false);
 
-    const handleSelectResult = (item) => {
-        setString(prevString => { return (prevString + (prevString !== "" ? ", " : "") + item[0].value) })
-        // setSelectedResults(prevList => {
-        //     return [...prevList, item[0]]
-        // })
-        // console.log(selectedResult)
-    };
+  const handleBackgroundSelection = (event) => {
+    setBackgroundSelected(event.target.checked);
+  };
 
-    return (
-        <div>
-            <VirtualizedSelect
-                placeholder={props.type === "move" ? "행동" : "장소"}
-                width="100%"
-                options={options}
-                onChange={(selectValue) => handleSelectResult(selectValue.map((item) => item))}
-                value={selectValue}
-                multi={true}
-            />
-        </div>
-    );
+  const handleSelectResult = (item) => {
+    setString((prevString) => {
+      return prevString + (prevString !== "" ? ", " : "") + item[0].value;
+    });
+    // setSelectedResults(prevList => {
+    //     return [...prevList, item[0]]
+    // })
+    // console.log(selectedResult)
+  };
+
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={backgroundSelected}
+            onChange={handleBackgroundSelection}
+          />
+          배경 선택
+        </label>
+      </div>
+      <VirtualizedSelect
+        placeholder={props.type === "move" ? "행동" : "장소"}
+        width="100%"
+        options={options}
+        onChange={(selectValue) =>
+          handleSelectResult(selectValue.map((item) => item))
+        }
+        value={selectValue}
+        multi={true}
+        disabled={backgroundSelected} // 드롭박스를 비활성화합니다
+      />
+    </div>
+  );
 };
 
 export default SearchComponent;
